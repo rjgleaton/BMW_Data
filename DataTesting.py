39234cb318a7bc9c41a4e1eb8a53e8c5c0a633bf
rjgleaton <70818603+rjgleaton@users.noreply.github.com>
2021-12-20 15:09:08 -0500

    Update DataTesting.py

--- DataTesting.py
+++ DataTesting.py
@@ -1,106 +1,88 @@
 from typing import List, Tuple, Set, Dict
-from xlwt import Workbook
 import pandas as pd
 import pdb as pdb
+from datetime import datetime, timedelta
+import datetime #one of the functions doesnt work unless I have this line, idk
 import openpyxl, math
 
+PATH: str = "MD04/MD04_10152021.xlsx"
+saftey_stock: int
+stock: int
+
 def main():
+    while(True):
+        #Want to work with xlsx files but data is currently stored as .csv, run this code to convert
+        #read_file = pd.read_csv(r"MD04/MD04 data until end of year test parts 10.15.21.csv")
+        #read_file.to_excel("MD04/MD04_10152021.xlsx", index = None, header = True)
 
-    #Justing using 4 files for beginning stage testing, want to keep it simple for now
-    path_list: list = ["130_Report/130_Report_1005.xlsx", "130_Report/130_Report_1006.xlsx","130_Report/130_Report_1008.xlsx","130_Report/130_Report_1011.xlsx"]
-
-    #For each file in the list, create a dictionary where keys are material ID (column 4 or E) data is list containing DoS, Saftey Stock (its ratio compared to stock), Supplier Location, change in DoS (initially 0), health status
-    #For each file update all items, note change in DoS, calculate and update health status, print each time
-
-    parts: Dict[str, list] = dict()
-    count = 0
-    for path in path_list:
-        count += 1
-        #Reading Excel file, using a work around because pd.read_excel was being a pain
-        wb = openpyxl.load_workbook(path)
+        wb = openpyxl.load_workbook(PATH)
         sheet = wb.active
         data = pd.DataFrame(sheet.values)
-        for index, row in data.iterrows():
-            #First row is just headers
-            if index == 0:
-                continue
 
-            label = row[4]
-            if row[31] != 0:
-                SS = row[32]/row[31]
-            else: 
-                SS = 0
+        material = input("What material do you want the health status for? ")
+        date = input ("For what date (mm/dd/yyyy)? ")
 
-            #TODO actually calculate how many days passed... currently just does a false version making use of the known excel sheet names
-            if label in parts:
-                change_in_days = int(path[22:26])-int(path_list[count-2][22:26]) 
-                change_DoS = (parts[label][0] - row[20])/change_in_days
-                if change_DoS < 0:
-                    change_DoS = 0
-            else: 
-                change_DoS = 0
-            #DoS, SS, Supplier Location, change in DoS, health status
-            parts[label] = [row[20], SS, row[37], change_DoS, calc_health(row[20], SS, row[37], change_DoS)]
+        #Find material at specified date, loop over that material for the next
+        #10 days and average out the values
+        avg: List = []
 
-        #pdb.set_trace()
-        writebook = Workbook()
-        sheet1 = writebook.add_sheet('Sheet 1')
-        #Write to column 49
-        for index, key in enumerate(parts):
-            #pdb.set_trace()
-            sheet1.write(index+1, 0, parts[key][4])
-        writebook.save("test%s.xls" % count) 
-    
-    pdb.set_trace()
-    
+        #Find last line index for that material and that date
+        df_filtered = data[data[0] == material]
 
-def calc_health(DoS: int, SS: int, supplier_loc: str, change_DoS: float) -> float:
-    #Base Case
-    if DoS <= 0:
-        return 0
-    
-    #All values chosen as 'optimal' and the amount by which a value can affect the score is more or less arbitrarily chosen at this point
-    initial_score: float = 30.0
-    
-    #We consider one week of supply to be optimal to begin with
-    if(DoS <= 7):
-        initial_score = initial_score*(DoS/7)
-
-    #Check for saftey stock
-    initial_score = SS_consideration(SS, supplier_loc, initial_score)
-
-    #Check to see if DoS is changing more than 1 per day
-    #Again just throw this into a sigmoid to make it scale as the value gets larger, k is set to -1 so it should act as a standard sigmoid function
-    #TODO probably should warn parts handler if this is the case aswell
-    if change_DoS > 1.0:
-        initial_score = initial_score-(0.2*sigmoid(change_DoS, k = -1))
-
-    return max(0.0, initial_score)
+        #Get saftey stock
+        ss_holder = df_filtered[df_filtered[12] == 2]
+        #Work around because I'm not super familiar with pandas tbh
+        for index, row in ss_holder.iterrows():
+            if(row[6] == "SafeSt"):
+                saftey_stock = abs(row[7])
+                break
+            #If not saftey stock value specified in file set to 0 by default
+            else:
+                saftey_stock = 0
+                break
 
 
-def SS_consideration(SS: int, supplier_loc: str, initial_score: float) -> float:
-    #We consider a ration of SS to stock of at least 5 to be optimal
-    #Base case for SS
-    if SS == 0:
-        #Check if supplier is close by
-        #TODO make this function actually consider distance to supplier, not just be a 'is it in america?' check
-        if supplier_loc == "United States":
-            initial_score = initial_score * 0.7
-        else: 
-            initial_score = initial_score * 0.5
-    #For now we just calculate this using a modified sigmoid function I stole from here: https://math.stackexchange.com/questions/1832177/sigmoid-function-with-fixed-bounds-and-variable-steepness-partially-solved
-    elif SS >= 5:
-        #Check if supplier is close by
-        #TODO make this function actually consider distance to supplier, not just be a 'is it in america?' check
-        if supplier_loc == "United States":
-            initial_score = initial_score * (1-(0.3*sigmoid(SS, k = -0.1)))
+        
+        #Sort data by only data on the date, get stock
+        '''
+        df_filtered_date = df_filtered[df_filtered[2] == date]
+        if(df_filtered_date.empty):
+            print("No data present for",date)
+            return
         else:
-            initial_score = initial_score * (1-(0.5*sigmoid(SS, k = -0.1)))
-    
-    return initial_score
+            stock_holder = df_filtered_date[df_filtered_date[12]==df_filtered_date[12].max()]
+            stock = stock_holder[8].values[0]
+            health_score = get_health_score(stock, saftey_stock)
+            avg.append(health_score)
+            print("Health Status of",material,"on",date,"is",health_score,", and stock is",stock)
+        '''
+        mm, dd, yyyy = map(int, date.split('/'))
+        date_obj = datetime.datetime(yyyy, mm, dd)
+
+        for i in range(10):
+            td = datetime.timedelta(days = i)
+            new_date = date_obj + td
+            formatted_date = datetime.date.strftime(new_date, "%m/%d/%Y")
+
+            df_filtered_date = df_filtered[df_filtered[2] == formatted_date]
+            if(df_filtered_date.empty):
+                print("No data present for",formatted_date)
+            else:
+                stock_holder = df_filtered_date[df_filtered_date[12]==df_filtered_date[12].max()]
+                stock = stock_holder[8].values[0]
+                health_score = get_health_score(stock, saftey_stock)
+                avg.append(health_score)
+                print("Health Status of",material,"on",date,"is",health_score,", and stock is",stock)
+
+        print("Average Health Status is",sum(avg)/len(avg))
+
+
+def get_health_score(stock: int, saftey_stock: int) -> float:
+    #Change value of k to affect attitude of curve
+    return sigmoid(SS = (stock/saftey_stock), k = 0.3)
 
 def sigmoid(SS: int, k: float) -> float:
-    return 1 - (1/(1+math.exp(-k*SS)))
+    return (((1/(1+math.exp(-k*SS)))-(1/2))*2)*100
 
 
 if __name__ == "__main__":
